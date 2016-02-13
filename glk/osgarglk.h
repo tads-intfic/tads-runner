b46cedcc43a40214ff11f4232b74b7ea9a3a158d
Chris Spiegel <cspiegel@gmail.com>
2016-02-12 17:08:00 -0800

    Update TADS to 3.1.3 / 2.5.17.
    This incorporates FrobTADS code to handle mostt OS-specific operations.
    
    See #203.

--- glk/osgarglk.h
+++ glk/osgarglk.h
@@ -1,6 +1,7 @@
 /******************************************************************************
  *                                                                            *
- * Copyright (C) 2011 by Ben Cressey.                                         *
+ * Copyright (C) 2006-2009 by Tor Andersson.                                  *
+ * Copyright (C) 2010 by Ben Cressey.                                         *
  *                                                                            *
  * This file is part of Gargoyle.                                             *
  *                                                                            *
@@ -20,7 +21,73 @@
  *                                                                            *
  *****************************************************************************/
 
-void os_init_ui_after_load(class CVmBifTable *, class CVmMetaTable *)
-{
-}
+#ifndef OSGARGLK_H
+#define OSGARGLK_H
 
+#define USE_OS_LINEWRAP	/* tell tads not to let os do paging */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdarg.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "osfrobtads.h"
+
+#define TADS_OEM_NAME   "Mr Oizo"
+
+/* Replace stricmp with strcasecmp */
+#define strnicmp strncasecmp
+#define stricmp strcasecmp
+
+#define memicmp os_memicmp
+
+int os_memicmp(const char *a, const char *b, int n);
+
+#define OS_SYSTEM_NAME "Gargoyle"
+
+#define OSFNMAX 1024
+
+#ifdef _WIN32
+#define OSPATHCHAR '\\'
+#define OSPATHALT "/:"
+#define OSPATHURL "\\/"
+#define OSPATHSEP ';'
+#define OS_NEWLINE_SEQ  "\r\n"
+#else
+#define OSPATHCHAR '/'
+#define OSPATHALT ""
+#define OSPATHURL "/"
+#define OSPATHSEP ':'
+#define OS_NEWLINE_SEQ "\n"
+#endif
+
+#define OSPATHPWD "."
+
+void os_put_buffer (unsigned char *buf, size_t len);
+void os_get_buffer (unsigned char *buf, size_t len, size_t init);
+unsigned char *os_fill_buffer (unsigned char *buf, size_t len);
+
+#define OS_MAXWIDTH 255
+
+#define OS_ATTR_HILITE  OS_ATTR_BOLD
+#define OS_ATTR_EM      OS_ATTR_ITALIC
+#define OS_ATTR_STRONG  OS_ATTR_BOLD
+
+#define OS_DECLARATIVE_TLS
+#define OS_DECL_TLS(t, v) t v
+
+int os_vasprintf(char **bufptr, const char *fmt, va_list ap);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OSGARGLK_H */
