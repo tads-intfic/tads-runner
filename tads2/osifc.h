973122ea493154f4d7236de39c095c0f37cd99ae
Adrian Welcker <ardi@diepixelecke.de>
2023-03-02 12:32:29 +0100

    Always define OS_ATTR_MONOSP

--- tads2/osifc.h
+++ tads2/osifc.h
@@ -2899,6 +2899,10 @@ void os_set_text_attr(int attr);
 /* attribute code: italic */
 #define OS_ATTR_ITALIC   0x0002
 
+/* attribute code: monospace
+ * (Introduced for the Glk port  -- AW 2023-01-20) */
+#define OS_ATTR_MONOSP   0x0004
+
 /*
  *   Abstract attribute codes.  Each platform can choose a custom rendering
  *   for these by #defining them before this point, in the OS-specific header
@@ -2930,12 +2934,6 @@ void os_set_text_attr(int attr);
 /* HTML <strong> attribute - by default, this has no effect */
 #ifndef OS_ATTR_STRONG
 # define OS_ATTR_STRONG  0
-#endif
-
-/* Monospaced text (HTML <tt> attribute - by default, this has no effect.
- * (Introduced for the Glk port  -- AW 2023-01-20) */
-#ifndef OS_ATTR_MONOSP
-# define OS_ATTR_MONOSP  0
 #endif
 
 
